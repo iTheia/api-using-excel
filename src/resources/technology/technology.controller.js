7c39697120c9b84879deab94e26364a67ea58655
Firelez <firelez@gmail.com>
2020-06-03 19:13:53 -0500

    second commit

--- src/resources/technology/technology.controller.js
+++ src/resources/technology/technology.controller.js
@@ -8,26 +8,62 @@ const technologyController = {
         const connection = await dbConnection()
         const query = util.promisify(connection.query).bind(connection)
 
-        const userExist = await query('select * from test')
-        /* verificar que el usuario no existe */
-        if(userExist){
-            res.send('user alredy exists')
-        }
-        /* THE INFO COMES FROM 'req.body' */
-        res.send('asd')
-    },
-    async sigIn(req, res){
-        
+        const data = req.body
+        const keys = Object.keys(data)
+
+        let columns = ''
+        keys.forEach((key, index) => {
+            columns += (index === keys.length -1)? key : `${key},`
+        })
+        let values = ''
+        keys.forEach((key, index) => {
+            values += (index === keys.length- 1)? data[key] : `${data[key]},`
+        })
+
+        const value = await query (`INSERT INTO TECHNOLOGY (${columns}) values (${values})`  )
+        res.send(value)
     },
     async update(req, res){
+        const connection = await dbConnection()
+        const query = util.promisify(connection.query).bind(connection)
         
+        const data = req.body
+        const keys = Object.keys(data)
+        const id = req.params.id
+
+        let set = ''
+        keys.forEach((key, index) => {
+            values += (index === keys.length- 1)? `${key} = '${data[key]}'` : `${key} = '${data[key]}',`
+        })
+
+        const value = await query ( `UPDATE TECHNOLOGY SET ${set} where id = ${id}` )
+        res.send(value)
     },
     async delete(req, res){
+        const connection = await dbConnection()
+        const query = util.promisify(connection.query).bind(connection)
+
+        const id = req.params.id
+        const value = await query ( `delete  TECHNOLOGY where id = ${id}` )
+
+        res.send(value)
         
     },
-    async getDashboard(req, res){
+    async getAll(req, res){
+        const connection = await dbConnection()
+        const query = util.promisify(connection.query).bind(connection)
         
-    }
+        const value = await query ( `SELECT * FROM TECHNOLOGY` )
+        res.send(value)
+    },
+    async getSingle(req, res){
+        const connection = await dbConnection()
+        const query = util.promisify(connection.query).bind(connection)
+        const id = req.params.id
+
+        const value = await query ( `SELECT * FROM TECHNOLOGY where id = ${id}` )
+        res.send(value)
+    },
 }
 
 export default technologyController
